446e314779721cb07157376778f89ddda5bb5efe
André Carvalho <andreunix@protonmail.com>
2022-08-29 23:14:46 -0400

    Tipos de dados: String

---
+++ javascript/basic/example-2/string.js
@@ -0,0 +1,23 @@
+/*
+* Cadeia de caracteres
+
+    "" // aspas duplas
+    '' // aspas simples
+    `` // template literals ou template strings
+*/
+
+console.log('"x"André Carvalho')
+console.log("'x'André Carvalho")
+//================================//
+console.log(`André " x   ' x ' "`)
+
+//texto multi linha e ultilização de variaveis 
+console.log(`
+
+André
+
+Carvalho
+
+`)
+
+console.log(`valor ${ 1 + 1 }`) //interpoloção 
